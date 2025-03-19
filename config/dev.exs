bab2bb4ceee8447a9c25228352dcce9dadd8a712
Nathan Shafer <nate@lotech.org>
2025-03-19 15:19:52 -0700

    Updated with phx_new 1.7.20

--- config/dev.exs
+++ config/dev.exs
@@ -16,8 +16,8 @@ config :myapp, Myapp.Repo,
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
+# Binding to loopback ipv4 address prevents access from other machines.
 config :myapp, MyappWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
