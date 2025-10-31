3db296b86ae1936f2426fcc43b0d8791a47ffd6d
Nathan Shafer <nate@lotech.org>
2025-10-31 13:40:33 -0700

    Fix tests

--- test/myapp_web/live/user_live/login_test.exs
+++ test/myapp_web/live/user_live/login_test.exs
@@ -9,39 +9,39 @@ defmodule MyappWeb.UserLive.LoginTest do
       {:ok, _lv, html} = live(conn, ~p"/users/log-in")
 
       assert html =~ "Log in"
-      assert html =~ "Register"
-      assert html =~ "Log in with email"
+      # assert html =~ "Register"
+      # assert html =~ "Log in with email"
     end
   end
 
-  describe "user login - magic link" do
-    test "sends magic link email when user exists", %{conn: conn} do
-      user = user_fixture()
+  # describe "user login - magic link" do
+  #   test "sends magic link email when user exists", %{conn: conn} do
+  #     user = user_fixture()
 
-      {:ok, lv, _html} = live(conn, ~p"/users/log-in")
+  #     {:ok, lv, _html} = live(conn, ~p"/users/log-in")
 
-      {:ok, _lv, html} =
-        form(lv, "#login_form_magic", user: %{email: user.email})
-        |> render_submit()
-        |> follow_redirect(conn, ~p"/users/log-in")
+  #     {:ok, _lv, html} =
+  #       form(lv, "#login_form_magic", user: %{email: user.email})
+  #       |> render_submit()
+  #       |> follow_redirect(conn, ~p"/users/log-in")
 
-      assert html =~ "If your email is in our system"
+  #     assert html =~ "If your email is in our system"
 
-      assert Myapp.Repo.get_by!(Myapp.Accounts.UserToken, user_id: user.id).context ==
-               "login"
-    end
+  #     assert Myapp.Repo.get_by!(Myapp.Accounts.UserToken, user_id: user.id).context ==
+  #              "login"
+  #   end
 
-    test "does not disclose if user is registered", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/users/log-in")
+  #   test "does not disclose if user is registered", %{conn: conn} do
+  #     {:ok, lv, _html} = live(conn, ~p"/users/log-in")
 
-      {:ok, _lv, html} =
-        form(lv, "#login_form_magic", user: %{email: "idonotexist@example.com"})
-        |> render_submit()
-        |> follow_redirect(conn, ~p"/users/log-in")
+  #     {:ok, _lv, html} =
+  #       form(lv, "#login_form_magic", user: %{email: "idonotexist@example.com"})
+  #       |> render_submit()
+  #       |> follow_redirect(conn, ~p"/users/log-in")
 
-      assert html =~ "If your email is in our system"
-    end
-  end
+  #     assert html =~ "If your email is in our system"
+  #   end
+  # end
 
   describe "user login - password" do
     test "redirects if user logs in with valid credentials", %{conn: conn} do
@@ -75,19 +75,19 @@ defmodule MyappWeb.UserLive.LoginTest do
     end
   end
 
-  describe "login navigation" do
-    test "redirects to registration page when the Register button is clicked", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/users/log-in")
+  # describe "login navigation" do
+  #   test "redirects to registration page when the Register button is clicked", %{conn: conn} do
+  #     {:ok, lv, _html} = live(conn, ~p"/users/log-in")
 
-      {:ok, _login_live, login_html} =
-        lv
-        |> element("main a", "Sign up")
-        |> render_click()
-        |> follow_redirect(conn, ~p"/users/register")
+  #     {:ok, _login_live, login_html} =
+  #       lv
+  #       |> element("main a", "Sign up")
+  #       |> render_click()
+  #       |> follow_redirect(conn, ~p"/users/register")
 
-      assert login_html =~ "Register"
-    end
-  end
+  #     assert login_html =~ "Register"
+  #   end
+  # end
 
   describe "re-authentication (sudo mode)" do
     setup %{conn: conn} do
@@ -99,11 +99,13 @@ defmodule MyappWeb.UserLive.LoginTest do
       {:ok, _lv, html} = live(conn, ~p"/users/log-in")
 
       assert html =~ "You need to reauthenticate"
-      refute html =~ "Register"
-      assert html =~ "Log in with email"
+      assert html =~ "Log in"
+      # refute html =~ "Register"
+      # assert html =~ "Log in with email"
 
+      # assert html =~ ~s(<input type="email" name="user[email]" id="login_form_magic_email" value="#{user.email}")
       assert html =~
-               ~s(<input type="email" name="user[email]" id="login_form_magic_email" value="#{user.email}")
+               ~s(<input type="email" name="user[email]" id="login_form_password_email" value="#{user.email}")
     end
   end
 end
